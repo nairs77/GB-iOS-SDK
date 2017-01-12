e0f8f41552b670d139b94f72fb60611c97e7fc99
nairs77 <nairs77@joyusers-Mac-mini.local>
2017-01-12 11:27:49 +0900

    update singleton

---
+++ GBSdk/GBSdk/Util/Singletone.h
@@ -0,0 +1,28 @@
+//
+//  Singletone.h
+//  GBSdk
+//
+//  Created by nairs77 on 2017. 1. 11..
+//  Copyright © 2017년 GeBros. All rights reserved.
+//
+
+#define SYNTHESIZE_SINGLETON_FOR_CLASS(classname, accessorname) \
++ (classname *)accessorname\
+{\
+static classname *accessorname = nil;\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+accessorname = [[classname alloc] init];\
+});\
+return accessorname;\
+}
+
+#define SINGLETON_FOR_CLASS(classname)\
++ (id) shared##classname {\
+static dispatch_once_t pred = 0;\
+static id _sharedObject = nil;\
+dispatch_once(&pred, ^{\
+_sharedObject = [[self alloc] init];\
+});\
+return _sharedObject;\
+}
